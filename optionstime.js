1bdccc00f29be7cf80fa61978a12824f82aea437
Maryala Phaneendra <mp2788@yp.com>
2016-10-03 12:53:42 -0700

    Fist push of the working plugin

---
+++ optionstime.js
@@ -0,0 +1,37 @@
+// Copyright 2016 Phaneendra Varma Maryala <pvmaryala@gmail.com>
+// Distributed under the terms of the GNU General Public License v2.
+// See http://www.gnu.org/licenses/gpl-2.0.txt for the full license text.
+
+function saveOptions() {
+	var timeformat = 24;
+	if(document.getElementById('t12').checked) {
+		var timeformat = 12;
+	}
+
+	localStorage['timeformat'] = timeformat;
+
+	var status = document.getElementById('status');
+	status.style['visibility'] = 'visible';
+
+	setTimeout(function() {status.style['visibility'] = 'hidden'}, 2000);
+}
+
+function restoreOptions() {
+	var timeformat = localStorage['timeformat'];
+
+	if(!timeformat) {
+		timeformat = 24;
+	}
+
+	var option = document.getElementById('t' + timeformat);
+
+	option.checked = true;
+}
+
+function startOptions() {
+	restoreOptions();
+
+	document.querySelector('#save').addEventListener('click', saveOptions);
+}
+
+window.onload = startOptions;
---
+++ timextension.js
@@ -0,0 +1,54 @@
+// Copyright 2016 Phaneendra Varma Maryala <pvmaryala@gmail.com>
+// Distributed under the terms of the GNU General Public License v2.
+// See http://www.gnu.org/licenses/gpl-2.0.txt for the full license text.
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June',
+	'July', 'August', 'September', 'October', 'November', 'December'];
+
+var timeformat = null;
+
+function padTime(x) {
+	if(x < 10) {
+		return '0' + x;
+	} else {
+		return x;
+	}
+}
+
+function updateTime() {
+	var date = new Date();
+
+	var h = date.getHours();
+	var m = date.getMinutes();
+	var s = date.getSeconds();
+
+	var dd = date.getDate();
+	var dm = date.getMonth();
+	var dy = date.getFullYear();
+
+	if(timeformat == 12) {
+		h = (h + 11) % 12 + 1;
+	}
+
+	var str_t = padTime(h) + '&nbsp;&nbsp;' + padTime(m) +
+		'&nbsp;&nbsp' + padTime(s);
+	var str_d = dd + ' ' + months[dm] + ' ' + dy;
+
+	document.getElementById('timeval').innerHTML = str_t;
+	document.getElementById('dateval').innerHTML = str_d;
+}
+
+function startTempo() {
+	timeformat = localStorage['timeformat'];
+
+	
+
+	updateTime();
+
+	setTimeout(function() {updateTime(); setInterval(updateTime, 1000)},
+		1000 - new Date().getTime() % 1000);
+}
+
+window.onload = startTempo;
+
+document.addEventListener('contextmenu', function(e) {e.preventDefault()});
